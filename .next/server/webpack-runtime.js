190e50fdbc519afb629368837964d2254966400d
delBull <marco.munoz9@gmail.com>
2024-11-15 10:41:18 -0600

    first commite

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e284e3fa2286a68a")
+/******/ 		__webpack_require__.h = () => ("e9399ad37bef5b8e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -1,101 +1,61 @@
-import { NftMint } from "@/components/token-mint";
+import { TokenMint } from "@/components/token-mint";
 import {
-	defaultChainId,
-	defaultNftContractAddress,
-	defaultTokenId,
+    defaultChainId,
+    defaultTokenContractAddress,
+    defaultTokenId,
 } from "@/lib/constants";
 import { client } from "@/lib/thirdwebClient";
 import { defineChain, getContract, toTokens } from "thirdweb";
 import { getContractMetadata } from "thirdweb/extensions/common";
-import {
-	getActiveClaimCondition as getActiveClaimCondition1155,
-	getNFT,
-	isERC1155,
-} from "thirdweb/extensions/erc1155";
-import { getCurrencyMetadata } from "thirdweb/extensions/erc20";
-import {
-	getActiveClaimCondition as getActiveClaimCondition721,
-	isERC721,
-} from "thirdweb/extensions/erc721";
 import { getActiveClaimCondition as getActiveClaimCondition20 } from "thirdweb/extensions/erc20";
+import { getCurrencyMetadata } from "thirdweb/extensions/erc20";
 
-// This page renders on the server
-// If you are looking for a client-rendered version, checkout src/page.tsx
+// Página SSR actualizada para Tokens
 export default async function Home() {
-	const tokenId = defaultTokenId;
-	const chain = defineChain(defaultChainId);
-	const contract = getContract({
-		address: defaultNftContractAddress,
-		chain,
-		client,
-	});
-	const [isERC721Query, isERC1155Query, contractMetadataQuery] =
-		await Promise.all([
-			isERC721({ contract }),
-			isERC1155({ contract }),
-			getContractMetadata({ contract }),
-		]);
+    const chain = defineChain(defaultChainId);
+    const contract = getContract({
+        address: defaultTokenContractAddress,
+        chain,
+        client,
+    });
 
-	const [nftQuery, claimCondition1155, claimCondition721, claimCondition20] =
-		await Promise.all([
-			isERC1155Query ? getNFT({ contract, tokenId }) : undefined,
-			isERC1155Query
-				? getActiveClaimCondition1155({ contract, tokenId })
-				: undefined,
-			isERC721Query ? getActiveClaimCondition721({ contract }) : undefined,
-			!isERC1155Query && !isERC721Query
-				? getActiveClaimCondition20({ contract })
-				: undefined,
-		]);
+    const [contractMetadataQuery, claimCondition20] = await Promise.all([
+        getContractMetadata({ contract }),
+        getActiveClaimCondition20({ contract }),
+    ]);
 
-	const displayName = isERC1155Query
-		? nftQuery?.metadata.name
-		: contractMetadataQuery.data?.name;
+    const displayName = contractMetadataQuery.data?.name || "";
+    const description = contractMetadataQuery.data?.description || "";
 
-	const description = isERC1155Query
-		? nftQuery?.metadata.description
-		: contractMetadataQuery.data?.description;
+    const priceInWei = claimCondition20?.pricePerToken;
+    const currency = claimCondition20?.currency;
 
-	const priceInWei = isERC1155Query
-		? claimCondition1155?.pricePerToken
-		: isERC721Query
-			? claimCondition721?.pricePerToken
-			: claimCondition20?.pricePerToken;
+    const currencyMetadata = currency
+        ? await getCurrencyMetadata({
+              contract: getContract({
+                  address: currency || "",
+                  chain,
+                  client,
+              }),
+          })
+        : undefined;
 
-	const currency = isERC1155Query
-		? claimCondition1155?.currency
-		: isERC721Query
-			? claimCondition721?.currency
-			: claimCondition20?.currency;
+    const currencySymbol = currencyMetadata?.symbol || "";
 
-	const currencyMetadata = currency
-		? await getCurrencyMetadata({
-				contract: getContract({
-					address: currency || "",
-					chain,
-					client,
-				}),
-			})
-		: undefined;
+    const pricePerToken =
+        currencyMetadata && priceInWei
+            ? Number(toTokens(priceInWei, currencyMetadata.decimals))
+            : null;
 
-	const currencySymbol = currencyMetadata?.symbol || "";
-
-	const pricePerToken =
-		currencyMetadata && priceInWei
-			? Number(toTokens(priceInWei, currencyMetadata.decimals))
-			: null;
-
-	return (
-		<NftMint
-			contract={contract}
-			displayName={displayName || ""}
-			contractImage={contractMetadataQuery.data?.image || ""}
-			description={description || ""}
-			currencySymbol={currencySymbol}
-			pricePerToken={pricePerToken}
-			isERC1155={!!isERC1155Query}
-			isERC721={!!isERC721Query}
-			tokenId={tokenId}
-		/>
-	);
+    return (
+        <TokenMint
+            contract={contract}
+            displayName={displayName}
+            contractImage={contractMetadataQuery.data?.image || ""}
+            description={description}
+            currencySymbol={currencySymbol}
+            pricePerToken={pricePerToken}
+            isERC20={true}
+        />
+    );
 }
